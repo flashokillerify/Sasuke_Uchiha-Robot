cffb173b7beba0edd87c6e6c649af1481c935ead
Flasho <75789819+flashokillerify@users.noreply.github.com>
2021-11-14 14:49:13 +0530

    Update logomaker.py

--- Cutiepii_Robot/modules/logomaker.py
+++ Cutiepii_Robot/modules/logomaker.py
@@ -35,7 +35,7 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
     fname2 = "LogoByCutiepii.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @Cutiepii_Robot")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @Nohaxrobot")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
